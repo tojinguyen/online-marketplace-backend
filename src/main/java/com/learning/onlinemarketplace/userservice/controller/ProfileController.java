c5c2ca8052737861cf5575a5e1a40373c53b0b59
tojinguyen <toainguyenprogramminggame@gmail.com>
2025-02-18 23:36:08 +0700

    create api

--- src/main/java/com/learning/onlinemarketplace/userservice/controller/ProfileController.java
+++ src/main/java/com/learning/onlinemarketplace/userservice/controller/ProfileController.java
@@ -1,4 +1,21 @@
 package com.learning.onlinemarketplace.userservice.controller;
 
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/profile")
 public class ProfileController {
+    @PostMapping("/create")
+    public void createProfile() {
+        // create profile
+    }
+
+    @PutMapping("/update")
+    public void updateProfile() {
+        // update profile
+    }
 }
